52d75c80463cc02371c06d595892dda35bbad73e
jmcae11-iscteiul <jmcae11@iscte-iul.pt>
2019-11-04 18:23:51 +0000

    project buid path fixes

--- ES_Project/src/userinterface/FirstScreen.java
+++ ES_Project/src/userinterface/FirstScreen.java
@@ -1,5 +1,9 @@
+package userinterface;
+
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class FirstScreen {
@@ -10,6 +14,16 @@ public class FirstScreen {
 	private JTextField file_path;
 	private JButton browser_button;
 	private JPanel panel;
+	
+	public FirstScreen() {
+		super();
+		this.frame = new JFrame("Software Engineering App");
+		this.browser = new JFileChooser();
+	}
+
+
+
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
