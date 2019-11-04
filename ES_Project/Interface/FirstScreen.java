7ef2ba9369f92c2458e18d7f29aece97bcde0c84
jmcae11-iscteiul <jmcae11@iscte-iul.pt>
2019-11-04 17:40:32 +0000

    Package creation and first screen class

---
+++ ES_Project/Interface/FirstScreen.java
@@ -0,0 +1,19 @@
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+
+public class FirstScreen {
+	
+	private JFrame frame;
+	private JFileChooser browser;
+	private JButton next_button;
+	private JTextField file_path;
+	private JButton browser_button;
+	private JPanel panel;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
