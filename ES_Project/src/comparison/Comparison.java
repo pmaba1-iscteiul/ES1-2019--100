c653b6ef4fc5e1abf6b4bb8a88ea69d2732866b7
albos-iscteiul <albos@iscte-iul.pt>
2019-11-06 13:44:15 +0000

    Comparison first commit

--- ES_Project/src/comparison/Comparison.java
+++ ES_Project/src/comparison/Comparison.java
@@ -1,5 +1,19 @@
 package comparison;
 
-public class Comparison {
+public class Comparison{	
+	
+/*	- DCI, Defeitos Corretamente Identificados, quando o valor da coluna correspondente 
+ 	à ferramenta (PMI ou iPlasma) é TRUE e a coluna	is_long_method também é TRUE;
+	- DII, Defeitos Incorretamente Identificados, quando o valor da coluna correspondente 
+	à ferramenta (PMI ou iPlasma) é TRUE e a coluna is_long_method é FALSE;
+	- ADCI, Ausências de Defeitos Corretamente Identificadas, quando o valor da coluna correspondente 
+	à ferramenta (PMI ou iPlasma) é FALSE e a coluna	is_long_method também é FALSE;
+	- ADII, Ausências de Defeitos Incorretamente Identificadas, quando o valor da coluna correspondente 
+	à ferramenta (PMI ou iPlasma) é FALSE e a colunais_long_method é TRUE.
+*/	
 
+	int dfv;
+	
+	
+	
 }
