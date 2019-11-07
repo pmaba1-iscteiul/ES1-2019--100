09ad4b0e0772c3e43194bd32e45d3783b24f45f1
fjsnr-iscteiul <fjsnr@iscte-iul.pt>
2019-11-07 22:58:34 +0000

    Alterações feitas em função de aumentar a performance

--- ES_Project/src/rules/BuildRules.java
+++ ES_Project/src/rules/BuildRules.java
@@ -1,4 +1,4 @@
-package rules;
+package francisco;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -6,9 +6,6 @@ import java.util.Iterator;
 /**
  * @author Francisco Raimundo
  *
- *Classe criada para então implementar a questão de ser true ou false
- *em funcionamento dos limites de cada linha do excel e das regras criadas
- *pelo utilizador.
  */
 
 public class BuildRules {
@@ -23,9 +20,9 @@ public class BuildRules {
 	ArrayList<Linha> arrayLinhas;
 
 	public static void main(String[] args) {
-		
+
 		BuildRules buildRules = new BuildRules();
-		
+
 		Linha linha = new Linha();
 		linha.setLOC(5);
 		linha.setCYCLO(5);
@@ -33,38 +30,38 @@ public class BuildRules {
 		linha.setLAA(5);
 		ArrayList<Linha> arrayLinhas = new ArrayList<>();
 		arrayLinhas.add(linha);
-		
+
 		ObjectVO objectVO = new ObjectVO();
 		objectVO.setMetric("LOC");
 		objectVO.setLimit(6);
 		objectVO.setOperator("<");
-		
+
 		ObjectVO objectVOO = new ObjectVO();
 		objectVOO.setMetric("ATFD");
 		objectVOO.setLimit(6);
 		objectVOO.setOperator(">");
-		
+
 		ObjectVO objectVOOO = new ObjectVO();
 		objectVOOO.setMetric("LAA");
 		objectVOOO.setLimit(6);
 		objectVOOO.setOperator("<");
-		
+
 		ArrayList<ObjectVO> arrayObjects = new ArrayList<>();
 		arrayObjects.add(objectVO);
 		arrayObjects.add(objectVOO);
 		arrayObjects.add(objectVOOO);
-		
+
 		ObjectRuleVO objectRuleVO = new ObjectRuleVO();
 		objectRuleVO.setListObjectsVO(arrayObjects);
-		
+
 		ArrayList<LogicOperator> arrayLogicsOperator = new ArrayList<>();
 		arrayLogicsOperator.add(LogicOperator.AND);
 		arrayLogicsOperator.add(LogicOperator.OR);
-		
+
 		objectRuleVO.setListOperators(arrayLogicsOperator);
-		
+
 		buildRules.getAndSetBoolean(objectRuleVO, arrayLinhas);
-		
+
 	}
 
 	public void getAndSetBoolean(ObjectRuleVO objectRuleVO, ArrayList<Linha> linha) {
@@ -88,133 +85,80 @@ public class BuildRules {
 
 	private boolean contasComOperadoresLogicos(ArrayList<LogicOperator> arrayLogicOperators) {
 
-		int i = 0;
-		boolean auxBoolean = false;
-		for(int iterador = 0; iterador!=arrayLogicOperators.size(); iterador++) {
-			if(i==0) {
-				System.out.println(arrayLogicOperators.get(iterador));
-				if(arrayLogicOperators.get(iterador).equals(LogicOperator.AND)) {
-					if(arrayIntermedio.get(i).equals(true) && arrayIntermedio.get(i+1).equals(true)) {
-						System.out.println(arrayIntermedio.get(i));
-						System.out.println(arrayIntermedio.get(i+1));
-						auxBoolean=true;
-					} else {
-						auxBoolean=false;
-					}
-				} else {
-					if(arrayIntermedio.get(i).equals(false) && arrayIntermedio.get(i+1).equals(false)) {
-						auxBoolean=false;
-					}
-					else {
-						auxBoolean=true;
-					}
-				}
-			} else {
-			if(arrayLogicOperators.get(iterador).equals(LogicOperator.AND)) {
-				if(auxBoolean==true && arrayIntermedio.get(i+1).equals(true)) {
-					auxBoolean=true;
-				} else {
-					auxBoolean=false;
-				}
-			} else {
-				if(arrayIntermedio.get(i).equals(false) && arrayIntermedio.get(i+1).equals(false)) {
-					auxBoolean=false;
-				}
-				else {
-					auxBoolean=true;
-				}
+		boolean auxBooleanIntermedio=false;
+		boolean auxBoolean=false;
+
+		for(int a=1; a != arrayLogicOperators.size()+1; a=a+1) {
+			if(arrayLogicOperators.size()==0) {
+				auxBoolean=arrayIntermedio.get(a);
 			}
+			auxBooleanIntermedio=arrayIntermedio.get(a);
+			auxBoolean=CalculaBoolean(auxBooleanIntermedio, arrayIntermedio.get(a), arrayLogicOperators.get(a-1));
+			auxBooleanIntermedio=auxBoolean;
 		}
-			//System.out.printf("Posição %d- %s\n", i, iterator.next());
-			i++;
-		}
-
 		return auxBoolean;
-}
-
-	private String extracted() {
-		return "AND";
 	}
 
-public void colocaNoArrayBooleans(Linha linha, ObjectVO a) {
-
-	auxFeature = a.getFeature();
-	auxLimit = a.getLimit();
-	auxOperator = a.getOperator();
-
-	switch (auxFeature) {
-	case "LOC":
-		// getLOC() -> guardo o valor do LOC do excel e vejo com o
-		// operador (< ou >) se dá true ou false
-
-		if (auxOperator.equals("<")) {
-
-			if (linha.getLOC() < auxLimit) {
-				arrayIntermedio.add(true);
+	private boolean CalculaBoolean(Boolean boolean1, Boolean boolean2, LogicOperator logicOperator) {
+		boolean auxBoolean=false;
+		if(logicOperator.equals(LogicOperator.AND)) {
+			if(boolean1.equals(true) && boolean2.equals(true)) {
+				System.out.println(boolean1);
+				System.out.println(boolean2);
+				auxBoolean=true;
 			} else {
-				arrayIntermedio.add(false);
+				auxBoolean=false;
 			}
 		} else {
-			if(linha.getLOC() > auxLimit) {
-				arrayIntermedio.add(true);
-			} else {
-				arrayIntermedio.add(false);
+			if(boolean1.equals(false) && boolean2.equals(false)) {
+				auxBoolean=false;
+			}
+			else {
+				auxBoolean=true;
 			}
 		}
-		break;
-
-	case "CYCLO":
-		// getCYCLO() -> guardo o valor do CYCLO do excel e vejo com o
-		// operador (< ou >) se dá true ou false
-		if(auxOperator.equals("<")) {
-
-			if( linha.getCYCLO() < auxLimit ) {
-				arrayIntermedio.add(true);
-			} else {
-				arrayIntermedio.add(false);
-			}	
-		}  else {
-			if(linha.getLOC() > auxLimit) {
-				arrayIntermedio.add(true);
-			} else {
-				arrayIntermedio.add(false);
-			}
-		}
-		break;
-
-	case "ATFD":
-		// getATFD() -> guardo o valor do ATFD do excel e vejo com o
-		// operador (< ou >) se dá true ou false
-		if(auxOperator.equals("<")) {
-
-			if( linha.getATFD() < auxLimit ) {
-				arrayIntermedio.add(true);
-			} else {
-				arrayIntermedio.add(false);
-			}
-		}  else {
-			if(linha.getLOC() > auxLimit) {
-				arrayIntermedio.add(true);
-			} else {
-				arrayIntermedio.add(false);
-			}
-		}
-		break;
-
-	case "LAA":
-		// getLAA() -> guardo o valor do LAA do excel e vejo com o
-		// operador (< ou >) se dá true ou false
-		if(auxOperator.equals("<")) {
-
-			if( linha.getLAA() < auxLimit ) {
-				arrayIntermedio.add(true);
-			} else {
-				arrayIntermedio.add(false);
-			}	
-		} 
-		break;
+		return auxBoolean;
 	}
 
-}
+
+	public void colocaNoArrayBooleans(Linha linha, ObjectVO a) {
+
+		double limiteNaLinha = getLimiteDaLinha(linha, a);
+		
+			if (a.getOperator().equals("<")) {
+				if (limiteNaLinha < a.getLimit()) {
+					arrayIntermedio.add(true);
+				} else {
+					arrayIntermedio.add(false);
+				}
+			} else {
+				if(limiteNaLinha > a.getLimit()) {
+					arrayIntermedio.add(true);
+				} else {
+					arrayIntermedio.add(false);
+				}
+			}
+			
+	}
+
+	private double getLimiteDaLinha(Linha l, ObjectVO a) {
+		Double auxDouble = 0.0;
+		try {
+			switch(a.getFeature()) {
+			case"LOC":
+				auxDouble = l.getLOC();
+			case"CYCLO":
+				auxDouble = l.getCYCLO();
+			case"ATFD":
+				auxDouble = l.getATFD();
+			case"LAA":
+				auxDouble = l.getLAA();
+		}
+			
+		} catch (Exception e) {
+			e.getMessage();
+		} 
+		return auxDouble;
+	}
 
 }
