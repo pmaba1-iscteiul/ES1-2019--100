d8c3d9870ff0e182cd5b4c68287a07e6b33ceebe
albos-iscteiul <albos@iscte-iul.pt>
2019-11-08 18:25:47 +0000

    Javadoc creation in both classes of quality package

--- ES_Project/src/quality/RulesQuality.java
+++ ES_Project/src/quality/RulesQuality.java
@@ -1,7 +1,17 @@
 package quality;
 
 import java.util.ArrayList;
+import excelReader.ExcelReader;
 import excelReader.Method;
+
+/**
+ * @author Alexandra Belo
+ * 
+ * Class that evaluates the rules/thresholds created/defined by the user to detect defects 
+ * relating to is_Long_Method or is_Feature_Envy.
+ * For this evaluation counters are used for each quality indicator. 
+ *
+ */
 
 public class RulesQuality {
 
@@ -9,28 +19,69 @@ public class RulesQuality {
 	private int DII = 0;	
 	private int ADCI = 0;
 	private int ADII = 0;
-	private ArrayList<Method> rows;
+	private ArrayList<java.lang.reflect.Method> rows;
 	
+	/**
+	 * RulesQuality constructor  
+	 * @param Object from somewhere else! /* objeto 
+	 */
 	public RulesQuality (/* objeto */) {
-		this.rows = /* as linhas do excel */;  
+		this.rows = /* as linhas do excel */new ArrayList<Method>();
+		this.compare();
 	}
 
+	/**
+	 * Returns the number of correctly identified defects by the rule
+	 * @return the DCI counter
+	 */
+	
 	public int getDCI() {
 		return DCI;
 	}
 
+	/**
+	 * Returns the number of incorrectly identified defects by the rule
+	 * @return the DII counter
+	 */
+	
 	public int getDII() {
 		return DII;
 	}
 
+	/**
+	 * Returns the number of correctly identified absence of defects by the rule
+	 * @return the ADCI counter
+	 */
+	
 	public int getADCI() {
 		return ADCI;
 	}
 
+	/**
+	 * Returns the number of incorrectly identified absence of defects by the rule
+	 * @return the ADII counter
+	 */
+	
 	public int getADII() {
 		return ADII;
 	}
 		
+// preciso de ver se é para comparar o resultado da regra com is_Long_Method ou com is_Feature_Envy
+//	private void compare(){
+//		for(Method m : rows) {
+//			
+//			
+//			if(m.is_Long_Method == true) {
+//				}
+//			else {
+//				
+//			}	
+//			
+//		}
+//	}
+	
+	
+	
 	
 	
 	
--- ES_Project/src/quality/ToolsQuality.java
+++ ES_Project/src/quality/ToolsQuality.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import excelReader.ExcelReader;
 import excelReader.Method;
 
+/**
+ * @author Alexandra Belo
+ *
+ * Class that evaluates iPlasma's and PMD's quality of defect detection of is_Long_Method.
+ * For this evaluation counters are used for each quality indicator. 
+ *
+ */
+
 public class ToolsQuality{	
 
 	private int PMD_DCI = 0;
@@ -15,44 +23,88 @@ public class ToolsQuality{
 	private int iPlasma_ADCI = 0;
 	private int iPlasma_ADII = 0;
 	private final ArrayList<Method> rows;
-
+	
+	/**
+	 * RulesQuality constructor  
+	 * @param Object from somewhere else! /* objeto 
+	 */
+	
 	public ToolsQuality(/* objeto */) {
-		this.rows = /* as linhas do excel */;  
+		this.rows = /* as linhas do excel */new ArrayList<Method>();  
 		this.compare();
 	}
 
+	/**
+	 * Returns the number of correctly identified defects by the tool PMD
+	 * @return the PMD_DCI counter
+	 */
+	
 	public int getPMD_DCI() {
 		return PMD_DCI;
 	}
 
+	/**
+	 * Returns the number of incorrectly identified defects by the tool PMD
+	 * @return the PMD_DII counter
+	 */
+	
 	public int getPMD_DII() {
 		return PMD_DII;
 	}
 
+	/**
+	 * Returns the number of correctly identified absence of defects by the tool PMD
+	 * @return the PMD_ADCI counter
+	 */
 	public int getPMD_ADCI() {
 		return PMD_ADCI;
 	}
-
+	
+	/**
+	 * Returns the number of incorrectly identified absence of defects by the tool PMD
+	 * @return the PMD_ADII counter
+	 */
+	
 	public int getPMD_ADII() {
 		return PMD_ADII;
 	}
-
+	
+	/**
+	 * Returns the number of correctly identified defects by the tool iPlasma
+	 * @return the PMD_DCI counter
+	 */
+	
 	public int getiPlasma_DCI() {
 		return iPlasma_DCI;
 	}
-
+	
+	/**
+	 * Returns the number of incorrectly identified defects by the tool iPlasma
+	 * @return the PMD_DII counter
+	 */
+	
 	public int getiPlasma_DII() {
 		return iPlasma_DII;
 	}
-
+	
+	/**
+	 * Returns the number of correctly identified absence of defects by the tool iPlasma
+	 * @return the PMD_ADCI counter
+	 */
+	
 	public int getiPlasma_ADCI() {
 		return iPlasma_ADCI;
 	}
-
+	
+	/**
+	 * Returns the number of incorrectly identified absence of defects by the tool iPlasma
+	 * @return the PMD_ADII counter
+	 */
+	
 	public int getiPlasma_ADII() {
 		return iPlasma_ADII;
 	}
-
+	
 	private void compare(){
 		for(Method m : rows) {
 			if(m.is_Long_Method == true) {
