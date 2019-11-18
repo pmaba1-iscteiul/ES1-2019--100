6e8170eedb638aca296c81f912eba601e91c8513
albos-iscteiul <albos@iscte-iul.pt>
2019-11-18 13:45:05 +0000

    ArrayList to List conversion

--- ES_Project/src/quality/ToolsQuality.java
+++ ES_Project/src/quality/ToolsQuality.java
@@ -1,6 +1,7 @@
 package quality;
 
-import java.util.ArrayList;
+import java.util.List;
+
 import excelReader.FileRow;
 import utils.DataBase;
 
@@ -22,15 +23,15 @@ public class ToolsQuality{
 	private int iPlasma_DII = 0;
 	private int iPlasma_ADCI = 0;
 	private int iPlasma_ADII = 0;
-	private ArrayList<FileRow> rows;
-	
+	private List<FileRow> rows;
+
 	/**
 	 * RulesQuality constructor  
 	 * @param DataBase object 
 	 */
-	
+
 	public ToolsQuality(DataBase db) {
-		this.rows.addAll(db.getExcel_file());  
+		this.rows = db.getExcel_file();  
 		this.compare();
 	}
 
@@ -38,7 +39,7 @@ public class ToolsQuality{
 	 * Returns the number of correctly identified defects by the tool PMD
 	 * @return the PMD_DCI counter
 	 */
-	
+
 	public int getPMD_DCI() {
 		return PMD_DCI;
 	}
@@ -47,7 +48,7 @@ public class ToolsQuality{
 	 * Returns the number of incorrectly identified defects by the tool PMD
 	 * @return the PMD_DII counter
 	 */
-	
+
 	public int getPMD_DII() {
 		return PMD_DII;
 	}
@@ -59,52 +60,52 @@ public class ToolsQuality{
 	public int getPMD_ADCI() {
 		return PMD_ADCI;
 	}
-	
+
 	/**
 	 * Returns the number of incorrectly identified absence of defects by the tool PMD
 	 * @return the PMD_ADII counter
 	 */
-	
+
 	public int getPMD_ADII() {
 		return PMD_ADII;
 	}
-	
+
 	/**
 	 * Returns the number of correctly identified defects by the tool iPlasma
 	 * @return the PMD_DCI counter
 	 */
-	
+
 	public int getiPlasma_DCI() {
 		return iPlasma_DCI;
 	}
-	
+
 	/**
 	 * Returns the number of incorrectly identified defects by the tool iPlasma
 	 * @return the PMD_DII counter
 	 */
-	
+
 	public int getiPlasma_DII() {
 		return iPlasma_DII;
 	}
-	
+
 	/**
 	 * Returns the number of correctly identified absence of defects by the tool iPlasma
 	 * @return the PMD_ADCI counter
 	 */
-	
+
 	public int getiPlasma_ADCI() {
 		return iPlasma_ADCI;
 	}
-	
+
 	/**
 	 * Returns the number of incorrectly identified absence of defects by the tool iPlasma
 	 * @return the PMD_ADII counter
 	 */
-	
+
 	public int getiPlasma_ADII() {
 		return iPlasma_ADII;
 	}
-	
+
 	private void compare(){
 		for(FileRow method : rows) {
 			if(method.isIs_Long_Method() == true) {
@@ -132,4 +133,21 @@ public class ToolsQuality{
 			}
 		}
 	}	
+
+	public static void main(String[] args) {
+		DataBase db = new DataBase("C:/Users/alexa/Documents/METI/ESI/projeto/Long-Method.xlsx");
+		//System.out.println(db.getExcel_file());
+		ToolsQuality tq = new ToolsQuality(db);
+		System.out.println("PDM:");
+		System.out.println("DCI =" + tq.getPMD_DCI());
+		System.out.println("DII =" + tq.getPMD_DII());
+		System.out.println("ADCI =" + tq.getPMD_ADCI());
+		System.out.println("ADII =" + tq.getPMD_ADII());
+		System.out.println("iPlasma:");
+		System.out.println("DCI =" + tq.getiPlasma_DCI());
+		System.out.println("DII =" + tq.getiPlasma_DII());
+		System.out.println("ADCI =" + tq.getiPlasma_ADCI());
+		System.out.println("ADII =" + tq.getiPlasma_ADII());
+		
+	}
 }
