9e77ef6922b0fd02d3dc155979c40a083fee6a1f
albos-iscteiul <albos@iscte-iul.pt>
2019-11-30 16:52:23 +0000

    getTools

--- ES_Project/src/utils/DataBase.java
+++ ES_Project/src/utils/DataBase.java
@@ -2,19 +2,29 @@ package utils;
 
 import java.util.List;
 
+
 import excelReader.ExcelReader;
 import excelReader.FileRow;
+import quality.ToolsQuality;
 
 public class DataBase {
 	
 	private List<FileRow>excel_file;
+	private ToolsQuality tools;
 
 	public DataBase(String path) {
 		super();
 		this.excel_file = ExcelReader.ReadFile(path);
+		this.tools = new ToolsQuality(this);
+
 	}
 
 	public List<FileRow> getExcel_file() {
 		return excel_file;
 	}
+	
+	public ToolsQuality getTools() {
+		return tools;
+	} 
+	
 }
